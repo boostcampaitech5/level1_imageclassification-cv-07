6d04876258233c9587bb2fa90c7a65aab467f15e
nstalways <nstalways@gmail.com>
2023-04-15 08:21:52 +0000

    [Modify] train.py script --model_dir

--- cv-07_image-classification/common/train.py
+++ cv-07_image-classification/common/train.py
@@ -286,7 +286,7 @@ if __name__ == '__main__':
 
     # Container environment
     parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_TRAIN', '/opt/ml/input/data/train/images'))
-    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR', './model'))
+    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR', '../model'))
 
     args = parser.parse_args()
     print(args)
